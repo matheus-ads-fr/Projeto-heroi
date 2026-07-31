0823dcee1dc923eab96f33acf03249e5567c0106
matheus-ads-fr <matheus.ads.fr@gmail.com>
2026-07-31 12:49:52 -0300

    Meu primeiro commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+let heroi = "Neguinho";
+let xp = 7500;
+let nivel = "";
+
+
+if (xp <= 1000) {
+    nivel = "Ferro";
+}else if (xp <= 2000) {
+    nivel = "bronze";
+}else if (xp <= 4000) {
+    nivel = "prata";
+}else if (xp <= 6000) {
+    nivel = "ouro";
+}else if (xp <= 8000) {
+    nivel = "Platina";
+}else if (xp <= 10000) {
+    nivel = "Imortal";
+}else if (xp >= 10.001) {
+    nivel = "Radiante";
+}
+
+console.log("o seu heroi " + heroi + "está no nivel " + nivel + ".");
+
